ccb74b18d65fdd89e025f248434a4f635fd8937d
Jax-Baiya <122954537+Jax-Baiya@users.noreply.github.com>
2023-03-15 19:32:19 +0300

    0-create_array.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,21 +9,22 @@
  */
 int count_words(char *s)
 {
-int num_words = 0;
-int in_word = 0;
+    int num_words = 0;
+    int in_word = 0;
+    int i;
 
-for (int i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == ' ')
-in_word = 0;
-else if (in_word == 0)
-{
-num_words++;
-in_word = 1;
-}
-}
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ')
+            in_word = 0;
+        else if (in_word == 0)
+        {
+            num_words++;
+            in_word = 1;
+        }
+    }
 
-return (num_words);
+    return (num_words);
 }
 
 /**
@@ -34,71 +35,64 @@ return (num_words);
  */
 char **strtow(char *str)
 {
-if (str == NULL || *str == '\0')
-return (NULL);
+    if (str == NULL || *str == '\0')
+        return (NULL);
 
-int num_words = count_words(str);
-if (num_words == 0)
-return (NULL);
+    int num_words = count_words(str);
+    if (num_words == 0)
+        return (NULL);
 
-char **words = malloc((num_words + 1) * sizeof(char *));
-if (words == NULL)
-return (NULL);
+    char **words;
+    int word_idx = 0;
+    int word_start = 0;
+    int word_len = 0;
+    int i, j;
 
-int word_idx = 0;
-int word_start = 0;
-int word_len = 0;
+    words = malloc((num_words + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
 
-for (int i = 0; str[i] != '\0'; i++)
-{
-if (str[i] == ' ')
-{
-if (word_len > 0)
-{
-char *new_word = malloc((word_len + 1) * sizeof(char));
-if (new_word == NULL)
-{
-for (int j = 0; j < word_idx; j++)
-free(words[j]);
-free(words);
-return (NULL);
-}
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            if (word_len > 0)
+            {
+                char *new_word = malloc((word_len + 1) * sizeof(char));
+                if (new_word == NULL)
+                {
+                    for (j = 0; j < word_idx; j++)
+                        free(words[j]);
+                    free(words);
+                    return (NULL);
+                }
 
-for (int j = 0; j < word_len; j++)
-new_word[j] = str[word_start + j];
+                for (j = 0; j < word_len; j++)
+                    new_word[j] = str[word_start + j];
 
-new_word[word_len] = '\0';
-words[word_idx] = new_word;
-word_idx++;
-word_len = 0;
-}
+                new_word[word_len] = '\0';
+                words[word_idx] = new_word;
+                word_idx++;
+                word_len = 0;
+            }
 
-word_start = i + 1;
-}
-else
-word_len++;
-}
+            word_start = i + 1;
+        }
+        else
+            word_len++;
+    }
 
-if (word_len > 0)
-{
-char *new_word = malloc((word_len + 1) * sizeof(char));
-if (new_word == NULL)
-{
-for (int j = 0; j < word_idx; j++)
-free(words[j]);
-free(words);
-return (NULL);
-}
+    if (word_len > 0)
+    {
+        char *new_word = malloc((word_len + 1) * sizeof(char));
+        if (new_word == NULL)
+        {
+            for (j = 0; j < word_idx; j++)
+                free(words[j]);
+            free(words);
+            return (NULL);
+        }
 
-for (int j = 0; j < word_len; j++)
-new_word[j] = str[word_start + j];
-
-new_word[word_len] = '\0';
-words[word_idx] = new_word;
-word_idx++;
-}
-
-words[word_idx] = NULL;
-return (words);
-}
+        for (j = 0; j < word_len; j++)
+            new_word[j] = str
 
